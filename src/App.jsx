941d7c4cbd8c26ac90ed37085074604d8e902799
minhnghialk <nghia13031995@gmail.com>
2024-11-20 10:06:08 +0700

    video 28

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,12 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+const MyComponent = () => {
+  return <div>Nguyễn Minh Nghĩa</div>;
+};
+
 const App = () => {
   const [count, setCount] = useState(0);
-
-  // function myFunction() {
-  //   console.log(">>> run my function");
-  // }
-
-  const myFunction = () => {
-    console.log(">>> run my function");
-  };
-
-  myFunction();
 
   return (
     <>
@@ -27,6 +21,7 @@ const App = () => {
         </a>
       </div>
       <h1>Hello wolrd & Nguyen Minh Nghia</h1>
+      <MyComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
