abb4568045596db27624b9ac73c1a028ec9f0495
minhnghialk <nghia13031995@gmail.com>
2024-11-20 15:08:52 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,10 +2,21 @@ import "./style.css";
 
 // JSX : 1 parent
 const MyComponent = () => {
+  const name = "Nguyễn Minh Nghĩa";
+  const age = 1995;
+
+  const array = [1, 2, 3];
+  const object = {
+    name: "Nguyễn Minh Nghĩa",
+    age: 1995,
+  };
   return (
     // fragment
     <>
-      <div>Nguyễn Minh Nghĩa</div>
+      <div>
+        {name}, {age}, {JSON.stringify(array)}, {object.name}, {object.age}
+      </div>
+      <div>{console.log("NGUYEN-MINH-NGHIA")}</div>
       <div className="child" style={{ textDecoration: "underline" }}>
         child
       </div>
