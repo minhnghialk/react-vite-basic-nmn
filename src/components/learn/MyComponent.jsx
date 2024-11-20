59ff2d4d83b1772321627566c11ac3bddfa479fb
minhnghialk <nghia13031995@gmail.com>
2024-11-20 14:54:48 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,5 +1,16 @@
+import "./style.css";
+
+// JSX : 1 parent
 const MyComponent = () => {
-  return <div>Nguyễn Minh Nghĩa</div>;
+  return (
+    // fragment
+    <>
+      <div>Nguyễn Minh Nghĩa</div>
+      <div className="child" style={{ textDecoration: "underline" }}>
+        child
+      </div>
+    </>
+  );
 };
 
 export default MyComponent;
