808babd0a9b1ce71e4dfca637a7a6479b0b58e73
minhnghialk <nghia13031995@gmail.com>
2024-11-21 21:19:52 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,12 +1,31 @@
 /* eslint-disable react/prop-types */
+
+// eslint-disable-next-line no-unused-vars
 const TodoNew = ({ handleAddNewTodo }) => {
-  handleAddNewTodo("Nghia");
+  // handleAddNewTodo("Nghia");
+
+  // const handleOnChange = (e) => {
+  //   console.log(">>> handle on change: ", e.target.value);
+  // };
+
+  const handleOnChange = (task) => {
+    console.log(">>> handle on change: ", task);
+  };
+
+  const handleClick = () => {
+    alert("Click me!");
+  };
 
   return (
     <div className="todo-new">
-      <input type="text" placeholder="Enter your task" />
+      <input
+        type="text"
+        placeholder="Enter your task"
+        // onChange={handleOnChange}
+        onChange={(e) => handleOnChange(e.target.value)}
+      />
 
-      <button onClick={handleAddNewTodo}>Add</button>
+      <button onClick={handleClick}>Add</button>
     </div>
   );
 };
